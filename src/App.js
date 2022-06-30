f0be01f60e047786014d15142f53646e8d4f9a90
Mawar Sharon Priskila <mawarsharon468@gmail.com>
2022-06-30 17:10:19 +0800

    menambahkan komponen dan page news dan info donasi

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ import HomeAboutUs from './components/HomeAboutUs/HomeAboutUs';
 import PageAboutUs from './components/PageAboutUs/PageAboutUs';
 import { FooterContainer } from './containers/footer'
 import ReqDonation from './components/ReqDonation/ReqDonation';
+import InfoDonasi from './components/InfoDonasi';
+import Berita from './components/News';
+import DonationArticle from './pages/AllDonation/InfoDonation';
+import NewsArticle from './pages/AllNews/News';
 
 //import AvatarTeam from './assets/AvatarTeam';
 
@@ -17,13 +21,21 @@ function App() {
       <Switch>
         <Route exact path='/'>
           <Heroo />
+          <InfoDonasi/>
+          <Berita/>
           <HomeAboutUs />
+        </Route>
+        <Route path='/AllNews'>
+          <NewsArticle/>
         </Route>
         <Route path='/ReqDonation'>
           <ReqDonation />
         </Route>
         <Route path='/PageAboutUs'>
           <PageAboutUs />
+        </Route>
+        <Route path='/AllDonationPage'>
+          <DonationArticle />
         </Route>
       </Switch>
       <FooterContainer />
