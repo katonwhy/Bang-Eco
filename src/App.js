bed4e64a9b414d9eee41f7818e0639fd8129011d
safiraa932 <merlansafira3@gmail.com>
2022-07-02 20:30:46 +0700

    style: responsive navbar & resize footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 // import Navbar from "./components/Navbar";
-import Navbar from "./components/navbar/navbar";
+import Navbar from "./components/Navbar/navbar";
 import Heroo from './components/Heroo/Heroo';
 import HomeAboutUs from './components/HomeAboutUs/HomeAboutUs';
 import PageAboutUs from './components/PageAboutUs/PageAboutUs';
