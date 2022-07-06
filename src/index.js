ed757ddbe5cf880b31723eb59161124cef4aeeed
MohammadAdjiFebriansyah <adji280201@gmail.com>
2022-07-06 23:06:12 +0700

    fix: fixed the file name

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './Styles/main.css';
+import './styles/main.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
