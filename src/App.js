560b6d7e05c540bf11430adbb0d1bcd238ae9649
Yoghi o <yoghioct@gmail.com>
2022-07-07 19:08:06 +0700

    fix redirect to /post

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import Navbar from "./components/navbar/navbar";
 import Hero from './components/Hero';
 import HomeAboutUs from './components/HomeAboutUs';
@@ -42,7 +42,7 @@ function App() {
         <Route path='/AllDonationPage'>
           <DonationArticle />
         </Route>
-
+        <Redirect to="/" />
       </Switch>
       <FooterContainer />
     </Router>
